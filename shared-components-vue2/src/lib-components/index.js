3944498b301937b89ce35042a8450d710fa68479
Kobi Peretz <57430438+MakeGood2you@users.noreply.github.com>
2024-12-17 20:30:13 +0200

    Delete shared-components-vue2 directory

--- shared-components-vue2/src/lib-components/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as DataMapping } from './DataMapping/DataMapping';
-export { default as JsonEditor } from './JsonEditor/JsonEditor';
-
-// Tests
-export { default as JsonEditorTest } from '../../dev/tests/JsonEditor.test';
-
-
